6c3df7aadd76743a21171a9688628284c4ec9bd9
Oleksii Kostiuk <oleksii.kostiuk@collect.ai>
2019-11-29 15:58:39 +0100

    flow + more battle mechanics

--- test/battle.spec.js
+++ test/battle.spec.js
@@ -1,4 +1,11 @@
-import { assert } from 'chai';
+const chai = require('chai');
+const chaiSubset = require('chai-subset');
+chai.use(chaiSubset);
+
+const { assert } = chai;
+
+import { DARK, FIRE, LIGHT, WATER, WIND } from "./elements";
+// import { ELEMENT_RELATIONS } from "../src/domain/skill-mechanics";
 // import {fight, Contestant, Theomars, MegaSmash} from '../src/index2';
 // import type {TargetSkill} from '../src/index2';
 // import {player, ai} from '../src/index';
@@ -56,14 +63,6 @@ const TowerMultipliers = {
     spd: [2, 3, 5, 6, 8, 9, 11, 12, 14, 15],
 };
 
-const Element = {
-    WIND: 'wind',
-    FIRE: 'fire',
-    WATER: 'water',
-    LIGHT: 'light',
-    DARK: 'dark',
-};
-
 const MAX_TOWER_LVL = 10;
 
 const GloryTowers = {
@@ -94,20 +93,22 @@ const GloryTowers = {
             cd: TowerMultipliers.cd[towers.cd - 1],
         };
 
+        res.currentHP = res.hp;
+
         switch (unit.element) {
-            case Element.WIND:
+            case WIND:
                 res.atk = res.atk + TowerMultipliers.wind_atk[towers.wind_atk - 1] * unit.atk / 100;
                 break;
-            case Element.FIRE:
+            case FIRE:
                 res.atk = res.atk + TowerMultipliers.fire_atk[towers.fire_atk - 1] * unit.atk / 100;
                 break;
-            case Element.WATER:
+            case WATER:
                 res.atk = res.atk + TowerMultipliers.water_atk[towers.water_atk - 1] * unit.atk / 100;
                 break;
-            case Element.LIGHT:
+            case LIGHT:
                 res.atk = res.atk + TowerMultipliers.light_atk[towers.light_atk - 1] * unit.atk / 100;
                 break;
-            case Element.DARK:
+            case DARK:
                 res.atk = res.atk + TowerMultipliers.dark_atk[towers.dark_atk - 1] * unit.atk / 100;
                 break;
         }
@@ -117,8 +118,26 @@ const GloryTowers = {
 };
 
 const GuildFlags = {
-    apply(unit) {
-        return {}
+
+    towers: {
+        "player-id": {
+            def: MAX_TOWER_LVL,
+            atk: MAX_TOWER_LVL,
+            hp: MAX_TOWER_LVL,
+            cd: MAX_TOWER_LVL,
+        }
+    },
+
+    apply(unit: Unit) {
+        const towers = this.towers[unit.player];
+
+        return {
+            cd: TowerMultipliers.cd[towers.cd - 1],
+            atk: TowerMultipliers.atk[towers.atk - 1] * unit.atk / 100,
+            def: TowerMultipliers.def[towers.def - 1] * unit.def / 100,
+            hp: TowerMultipliers.hp[towers.hp - 1] * unit.hp / 100,
+            currentHP: TowerMultipliers.hp[towers.hp - 1] * unit.hp / 100,
+        };
     }
 };
 
@@ -137,8 +156,22 @@ function defaultRng(): number {
 }
 
 type rng = () => number;
-function hit(roll: rng = defaultRng) {
 
+function hit(roll: rng = defaultRng) {
+    let chance = roll();
+    // todo: add elemental king effect = always advantage
+    // const glancing_chance = ELEMENT_RELATIONS[context.caster.element].weak === context.target.element ? 15 : 0;
+    // const crit_mod = hasAdvantage(context.caster, context.target) ? 15 : 0;
+    //
+    // if (n <= (glancing_chance + context.caster.glancing_mod)) {
+    //     return new GlancingDamageStrategy(context);
+    // } else if (this.roll() <= context.caster.cr + crit_mod) {
+    //     return new CritDamageStrategy(context);
+    // } else if (hasAdvantage(context.caster, context.target) && this.roll() <= 15) {
+    //     return new CrushingDamageStrategy(context);
+    // }
+    //
+    // return new NormalDamageStrategy(context);
 }
 
 
@@ -146,10 +179,9 @@ const GenericSkills: Map<number, SkillSpec> = {
     1: {
         target: targetEnemy,
         action: [
-            function (caster:Contestant, target: Contestant, params: SkillMeta) {
-                // raw_dmg * (100 + this.context.skill.power) / 100;
+            function (caster: Contestant, target: Contestant, params: SkillMeta) {
                 const rawDmg = caster.atk * 3.3 + 35;
-                target.dmg(rawDmg * (100 + params.dmgMultiplier) / 100 );
+                target.dmg(rawDmg * (100 + params.dmgMultiplier) / 100);
             }
         ],
         meta: {
@@ -201,6 +233,7 @@ class GenericSkill implements Ability, RefreshableAbility {
     meta: SkillMeta;
     target: Targeter;
     apply: BattleMechanic;
+    cooldown: number = 0;
 
     constructor(meta: SkillMeta, target: Targeter, apply: BattleMechanic) {
         this.meta = meta;
@@ -213,8 +246,12 @@ class GenericSkill implements Ability, RefreshableAbility {
         this.cooldown = this.meta.cooldown;
     }
 
-    refresh() {
-        this.cooldown = 0;
+    refresh(turns: number) {
+        if (turns > 0) {
+            this.cooldown = Math.max(0, this.cooldown - turns);
+        } else {
+            this.cooldown = 0;
+        }
     }
 }
 
@@ -222,27 +259,93 @@ interface Observer {
     subscribe(battle: Battle): void;
 }
 
+class BattleStarted {
+    constructor(battleId, type, teamA, teamB) {
+        this.id = battleId;
+        this.type = type;
+        this.teamA = teamA;
+        this.teamB = teamB;
+    }
+}
+
 class TurnStarted {
     constructor(unit: Contestant) {
         this.id = unit.id;
         this.atb = 0;
+        this.cooldowns = unit.skills.map(s => {
+            s.refresh(1);
+            return s.cooldown;
+        });
     }
 }
 
+class Tick {
+    constructor(unit: Contestant) {
+        this.id = unit.id;
+        this.atb = unit.atb + Math.round(unit.spd * 0.07);
+    }
+}
+
+class GloryTowersApplied {
+    constructor(unit: Contestant) {
+        this.id = unit.id;
+        Object.assign(this, GloryTowers.apply(unit));
+    }
+}
+
+class GuildFlagsApplied {
+    constructor(unit: Contestant) {
+        this.id = unit.id;
+        Object.assign(this, GuildFlags.apply(unit));
+    }
+}
+
+class DmgReceived {
+    constructor(unit: Contestant, dmg) {
+        this.id = unit.id;
+        this.currentHP = Math.max(0, Math.round(unit.currentHP - dmg));
+    }
+}
+
+class SkillCasted {
+    constructor(unit: Contestant, skillId) {
+        this.id = unit.id;
+        this.cooldowns = {
+            [skillId]: unit.skills[skillId].cooldown
+        };
+    }
+}
+
+const BTL = Symbol('battle');
+
 class Contestant {
 
+    id: string;
+    element: WIND | FIRE | WATER | LIGHT | DARK;
+    player: string;
+
+    currentHP: number;
+    atb: number;
+
+    hp: number;
+    atk: number;
+    def: number;
+    spd: number;
+    cr: number;
+    cd: number;
+    res: number;
+    acc: number;
+
     constructor(battle, unit: Unit) {
-        this.battle = battle;
+        this[BTL] = battle;
         const { id, element, player, skills, ...stats } = unit;
 
         this.id = id;
-        //
         this.element = element;
         this.player = player;
         //
-        for (let s in stats) {
-            this[s] = stats[s];
-        }
+        Object.assign(this, stats);
+        this.currentHP = this.hp;
         //
         this.atb = 0;
         this.skills = skills.map(GetSkill);
@@ -250,59 +353,64 @@ class Contestant {
         // this.subscribePassives(battle);
     }
 
+    useSkill(skillId, targetId, units) {
+        const skill = this.skills[skillId];
+        skill.cast(this, targetId, units);
+        this[BTL].causes(new SkillCasted(this, skillId));
+    }
+
     tick() {
-        this.causes({
-            eventType: "tick",
-            unit: {
-                id: this.id,
-                atb: this.atb + Math.round(this.spd * 0.07),
-            }
-        });
+        this[BTL].causes(
+            new Tick(this)
+        );
     }
 
     startTurn() {
-        this.battle.causes({
-            eventType: "turn-started",
-            unit: {
-                id: next.id,
-                atb: 0,
-                cooldowns: this.skills.forEach(s => s.refresh(1)).map(s => s.cooldown),
-            }
-        });
-    }
-
-    apply(unit: Stats) {
-        const { cooldowns, effects, ...stats } = unit;
-
-        if (effects) {
-            this.effects = effects;
-        }
-
-        if (cooldowns) {
-            for (const c in cooldowns) {
-                this.skills[c].cooldown = cooldowns[c];
-            }
-        }
-
-        if (stats) {
-            for (const s in stats) {
-                this[s] = stats[s];
-            }
-        }
-    }
-
-    subscribePassives(battle) {
-        this.skills.forEach(s => s.subscribe(battle));
+        this[BTL].causes(
+            new TurnStarted(this)
+        );
     }
 
     dmg(amount) {
-        this.battle.causes({
-            eventType: "dmg",
-            unit: {
-                id: this.id,
-                hp: Math.round(this.hp - amount),
+        this[BTL].causes(
+            new DmgReceived(this, amount)
+        );
+    }
+
+    applyPermanentStats(extraStats: Unit) {
+        const {id, ...stats} = extraStats;
+        for (const i in stats) {
+            this[i] += stats[i];
+        }
+    }
+
+    apply(event: Object) {
+        const { id, cooldowns, ...stats } = event;
+
+        if (cooldowns) {
+            for (const i in cooldowns) {
+                this.skills[i].cooldown = cooldowns[i];
             }
-        });
+        }
+
+        Object.assign(this, stats);
+    }
+}
+
+class SkillUsed {
+    constructor(skillId, targetId) {
+        this.skillId = skillId;
+        this.targetId = targetId;
+    }
+}
+
+class CurrentActiveUnit {
+    constructor(unit: Contestant) {
+        this.id = unit.id;
+        this.skills = Object.keys(unit.skills).reduce((skills, skillId) => {
+            skills[skillId] = unit.skills[skillId].cooldown;
+            return skills;
+        }, {});
     }
 }
 
@@ -316,7 +424,7 @@ class Battle {
     static TOA: string = 'toa';
     //
     static ATB_SIZE = 100;
-
+    //
     id: string;
     version: number = 0;
     events: [];
@@ -331,37 +439,24 @@ class Battle {
         this.events = [];
 
         this.applyTowers = this.applyTowers.bind(this);
-        this.turnStarted = [];
     }
 
     applyTowers(unit: Unit) {
-        this.causes({
-            eventType: 'glory-towers',
-            unit: {
-                id: unit.id,
-                ...GloryTowers.apply(unit),
-            },
-        });
-
+        this.causes(new GloryTowersApplied(unit));
         if (this.type === Battle.GUILD_BATTLE) {
-            this.causes({
-                eventType: 'glory-towers',
-                unit: {
-                    id: unit.id,
-                    ...GuildFlags.apply(unit),
-                },
-            });
+            this.causes(new GuildFlagsApplied(unit));
         }
     }
 
     start(teamA: Unit[], teamB: Unit[], type) {
-        this.causes({ eventType: 'battle-started', type, teamA, teamB });
+        this.causes(new BattleStarted(this.id, type, teamA, teamB));
     }
 
     next() {
-        this.units.forEach(unit => unit.tick());
+        const units = Object.values(this.units);
+        units.forEach(unit => unit.tick());
 
-        const next = Object.values(this.units)
+        const next = units
             .filter(u => u.atb >= Battle.ATB_SIZE)
             .sort((a, b) => a.atb - b.atb)
             .pop();
@@ -372,39 +467,43 @@ class Battle {
 
         next.startTurn();
 
-        return true;
+        return new CurrentActiveUnit(next);
     }
 
     cast(skillIdx, targetId) {
-        const skill = Skills.get(this.current.skills[skillIdx]);
-        skill.cast(this.current, this, targetId);
+        if (this.current.skills[skillIdx].cooldown === 0) {
+            this.causes(new SkillUsed(skillIdx, targetId));
+            return;
+        }
+        throw new Error("Skill is on CD");
     }
 
-    when({ eventType, ...payload }) {
-        switch (eventType) {
-            case "prepared":
-                this.type = payload.type;
-                break;
-            case "battle-started":
+    when(event: Object) {
+        switch (event.constructor) {
+            case BattleStarted:
                 this.started = true;
-                this.type = payload.type;
-                [...payload.teamA, ...payload.teamB].forEach((unit) => {
+                this.type = event.type;
+                [...event.teamA, ...event.teamB].forEach((unit) => {
                     this.units[unit.id] = new Contestant(this, unit);
                     this.applyTowers(unit);
                 });
                 break;
-            case "turn-started":
-                this.current = this.units[payload.unit.id];
-                this.current.startTurn();
-                this.turnStarted.forEach(listener => listener(this));
+            case SkillUsed:
+                this.current.useSkill(event.skillId, event.targetId, this.units);
                 break;
-            case "glory-towers":
-            case "dmg":
-                console.log(payload.unit.id);
-                this.units[payload.unit.id].apply(payload.unit);
+            case GloryTowersApplied:
+            case GuildFlagsApplied:
+                this.units[event.id].applyPermanentStats(event);
+                break;
+            case TurnStarted:
+                this.current = this.units[event.id];
+            case DmgReceived:
+            case Tick:
+            case SkillCasted:
+                this.units[event.id].apply(event);
                 break;
             default:
-                throw new Error(`Unknown Event: "${eventType}"`);
+                throw new Error(`Unknown Event: "${event.constructor.name}"`);
         }
     }
 
@@ -417,7 +516,6 @@ class Battle {
         this.events.push(event);
         this.applyEvent(event);
     }
-
 }
 
 describe('Battle', () => {
@@ -426,9 +524,9 @@ describe('Battle', () => {
 
         const slowUnit = {
             id: 'slow-unit-id',
-            element: 'water',
+            element: WATER,
             player: 'player-id',
-            hp: 1500,
+            hp: 1000,
             atk: 100,
             def: 100,
             spd: 100,
@@ -440,9 +538,9 @@ describe('Battle', () => {
         };
         const fastUnit = {
             id: 'fast-unit-id',
-            element: 'wind',
+            element: WIND,
             player: 'player-id',
-            hp: 1500,
+            hp: 1000,
             atk: 100,
             def: 100,
             spd: 116,
@@ -455,87 +553,50 @@ describe('Battle', () => {
 
         battle.start([slowUnit], [fastUnit], Battle.GUILD_BATTLE);
 
-        assert.deepEqual(battle.type, "guild_battle");
-        assert.deepEqual(battle.units, {
-            'slow-unit-id': {
-                id: 'slow-unit-id',
-                player: 'player-id',
-                element: 'water',
-                atb: 0,
-                base: {
-                    hp: 1800,
-                    atk: 140,
-                    def: 120,
-                    spd: 115,
-                    cr: 15,
-                    cd: 75,
-                    res: 15,
-                    acc: 0,
-                },
-                hp: 1800,
-                atk: 140,
-                def: 120,
-                spd: 115,
-                cr: 15,
-                cd: 75,
-                res: 15,
-                acc: 0,
-                skills: [
-                    {
-                        dmgMultiplier: 0,
-                        effectMultiplier: 0,
-                        maxCooldown: 0,
-                        сooldown: 0,
-                    }
-                ]
+        assert.deepEqual(battle.type, Battle.GUILD_BATTLE);
 
-            },
-            'fast-unit-id': {
-                id: 'fast-unit-id',
-                player: 'player-id',
-                element: 'wind',
-                atb: 0,
-                base: {
-                    hp: 1800,
-                    atk: 140,
-                    def: 120,
-                    spd: 133.4,
-                    cr: 15,
-                    cd: 75,
-                    res: 15,
-                    acc: 0,
-                },
-                hp: 1800,
-                atk: 140,
-                def: 120,
-                spd: 133.4,
-                cr: 15,
-                cd: 75,
-                res: 15,
-                acc: 0,
-                skills: [
-                    {
-                        dmgMultiplier: 0,
-                        effectMultiplier: 0,
-                        maxCooldown: 0,
-                        сooldown: 0,
-                    }
-                ]
-            }
+        assert.include(battle.units['slow-unit-id'], {
+            id: 'slow-unit-id',
+            player: 'player-id',
+            element: WATER,
+            atb: 0,
+            currentHP: 1400,
+            hp: 1400,
+            atk: 160,
+            def: 140,
+            spd: 115,
+            cr: 15,
+            cd: 100,
+            res: 15,
+            acc: 0,
         });
 
-        battle.next();
+        assert.include(battle.units['fast-unit-id'], {
+            id: 'fast-unit-id',
+            player: 'player-id',
+            element: WIND,
+            atb: 0,
+            currentHP: 1400,
+            hp: 1400,
+            atk: 160,
+            def: 140,
+            spd: 133.4,
+            cr: 15,
+            cd: 100,
+            res: 15,
+            acc: 0,
+        });
 
-        assert.equal(battle.current.id, 'fast-unit-id', 'fastest unit should get a turn');
-        assert.equal(battle.current.atb, 0, 'atb should be reseted when turn starts');
+        const nextUnit = battle.next();
+
+        assert.deepEqual(nextUnit, { id: 'fast-unit-id', skills: { 0: 0 } }, 'fastest unit should get a turn');
 
         battle.cast(0, 'slow-unit-id');
 
-        assert.equal(battle.units['slow-unit-id'].hp, 1799);
+        assert.equal(battle.units['slow-unit-id'].currentHP, 835);
     });
 
-
-    describe.only('Generic Skill', () => {
+    describe('Generic Skill', () => {
         it('casts dmg to target', () => {
             const skill = GetSkill(1);
 
@@ -569,22 +630,15 @@ describe('Battle', () => {
                 acc: 0,
                 skills: [1],
             });
+
             battle.units = {
                 'caster-id': caster,
                 'target-id': target,
             };
+
             skill.cast(caster, 'target-id', battle.units);
 
-            assert.deepEqual([...battle.events],[
-                {
-                    eventType: "dmg",
-                    unit: {
-                        id: 'target-id',
-                        hp: 634,
-                    }
-                },
-            ]);
+            assert.deepEqual(battle.units['target-id'].currentHP, 634);
         });
     });
-
 });
